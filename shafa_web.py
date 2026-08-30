366d9ccb7a4d9a8c2b3db838643bf6e948705fd8
astrelsba <astrelsba@gmail.com>
2026-08-30 23:20:05 +0300

    1.4

--- shafa_web.py
+++ shafa_web.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+def minimum_commission(price):
+    # Мінімальна комісія залежно від суми замовлення
+    if price <= 50:
+        return 10
+    elif price <= 100:
+        return 20
+    else:
+        return 30
+
 def calculate_commission(price, category):
     if category in ['Жіночий', 'Чоловічий', 'Дитячий', 'Аксесуари', 'Краса Та Здоров', 'Для Дому']:
         if price <= 500:
@@ -16,8 +25,9 @@ def calculate_commission(price, category):
         else:
             percent = 9
     actual_comm = price * percent / 100
-    commission = max(30, min(500, actual_comm))
-    return percent, commission, actual_comm
+    min_comm = minimum_commission(price)
+    commission = max(min_comm, min(500, actual_comm))
+    return percent, commission, actual_comm, min_comm
 
 st.markdown('<div style="font-size: 2em; font-weight: bold; user-select: none;">Калькулятор комісії SHF</div>', unsafe_allow_html=True)
 
@@ -35,15 +45,15 @@ if st.button("Розрахувати"):
             st.error("Необхідна ціна продажу")
         else:
             output = ""
-            percent_price, comm_price, actual_comm_price = calculate_commission(price, category)
-            highlight_price = actual_comm_price < 30 or actual_comm_price > 500
+            percent_price, comm_price, actual_comm_price, min_comm_price = calculate_commission(price, category)
+            highlight_price = actual_comm_price < min_comm_price or actual_comm_price > 500
             comm_str_price = f"{comm_price:.2f} грн"
             if highlight_price:
                 comm_str_price += f" | <span style='color: yellow; font-size: 14px;'>фактична {actual_comm_price:.2f} грн</span>"
             output += f"**Для ціни продажу ({price} грн):**\n- Відсоток: {percent_price}%\n- Сума списання: {comm_str_price}\n\n"
             if actual_price > 0:
-                percent_actual, comm_actual, actual_comm_actual = calculate_commission(actual_price, category)
-                highlight_actual = actual_comm_actual < 30 or actual_comm_actual > 500
+                percent_actual, comm_actual, actual_comm_actual, min_comm_actual = calculate_commission(actual_price, category)
+                highlight_actual = actual_comm_actual < min_comm_actual or actual_comm_actual > 500
                 comm_str_actual = f"{comm_actual:.2f} грн"
                 if highlight_actual:
                     comm_str_actual += f" | <span style='color: yellow; font-size: 14px;'>фактична {actual_comm_actual:.2f} грн</span>"
